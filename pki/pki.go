1ee5e0ee93fd3f2b5ef6eaf913bc9b8183d0efbe
Tai Groot <tai@taigrr.com>
2021-08-20 21:51:20 -0700

    adds Sprout ID verification + tests

--- pki/pki.go
+++ pki/pki.go
@@ -3,11 +3,16 @@ package pki
 import (
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	. "github.com/gogrlx/grlx/config"
 )
 
+var sproutMatcher *regexp.Regexp
+
 func init() {
+	sproutMatcher = regexp.MustCompile(`^[0-9a-z\.][-0-9a-z\.]*$`)
 
 }
 
@@ -62,4 +67,45 @@ func SetupPKISprout() {
 		log.Panicf(err.Error())
 	}
 
+}
+
+// rules on sprout ids:
+// must be unique
+// if multiple sprouts claim the same id, the first one gets the id,
+// following sprouts get id_n where n is their place in the queue
+// sprout ids must be valid *nix hostnames: [0-9a-z\.][-0-9a-z\.]
+// sprout ids cannot have underscores in their names, sprout daemons
+// should automatically convert any found underscores to hyphens, unless
+// the hostname starts with an underscore, in which case it is removed.
+// maximum length is 253 characters
+// trailing dots are not allowed
+
+func CreateSproutID() string {
+	id, err := os.Hostname()
+	if err != nil {
+		// TODO don't panic, use another method of derivation
+		panic(err)
+	}
+	id = strings.ReplaceAll(id, "_", "-")
+	id = strings.TrimPrefix(id, "-")
+	return id
+}
+
+func IsValidSproutID(id string) bool {
+	if strings.Contains(id, "_") {
+		return false
+	}
+	if strings.HasPrefix(id, "-") {
+		return false
+	}
+	if strings.HasSuffix(id, ".") {
+		return false
+	}
+	if !sproutMatcher.MatchString(id) {
+		return false
+	}
+	if len(id) < 254 {
+		return true
+	}
+	return false
 }
---
+++ pki/pki_test.go
@@ -0,0 +1,31 @@
+package pki
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestIsValidSproutID(t *testing.T) {
+	testCases := []struct {
+		id            string
+		shouldSucceed bool
+		testID        string
+	}{
+		{id: "test", shouldSucceed: true, testID: "test"},
+		{id: "-test", shouldSucceed: false, testID: "leading hyphen"},
+		{id: "te_st", shouldSucceed: false, testID: "embedded underscore"},
+		{id: "grlxNode", shouldSucceed: false, testID: "capital letter"},
+		{id: "t.est", shouldSucceed: true, testID: "embedded dot"},
+		{id: strings.Repeat("a", 300), shouldSucceed: false, testID: "300 long string"},
+		{id: strings.Repeat("a", 253), shouldSucceed: true, testID: "253 long string"},
+		{id: "0132-465798qwertyuiopasdfghjklzxcv.bnm", shouldSucceed: true, testID: "keyboard smash"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.testID, func(t *testing.T) {
+			if IsValidSproutID(tc.id) != tc.shouldSucceed {
+				t.Errorf("`%s`: expected %v but got %v", tc.id, tc.shouldSucceed, !tc.shouldSucceed)
+			}
+		})
+
+	}
+}
