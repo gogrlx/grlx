4361f5b315bd50c637714d76f2b62dc59a8b6e53
Tai Groot <tai@taigrr.com>
2021-09-26 22:02:29 -0700

    move rootball into repo

--- types/errors.go
+++ types/errors.go
@@ -3,16 +3,17 @@ package types
 import "errors"
 
 var (
-	ErrAPIRouteNotFound  = errors.New("API Route not found.")
-	ErrAlreadyAccepted   = errors.New("This Sprout ID was already accepted.")
-	ErrAlreadyDenied     = errors.New("This Sprout ID was already denied.")
-	ErrAlreadyRejected   = errors.New("This Sprout ID was already rejected.")
-	ErrAlreadyUnaccepted = errors.New("This Sprout ID was already unaccepted.")
-	ErrCannotParseRootCA = errors.New("Cannot load the RootCA certificate.")
-	ErrSproutIDFound     = errors.New("A Sprout ID matching that system has already been recorded.")
-	ErrSproutIDInvalid   = errors.New("Bad user input: invalid SproutID received")
-	ErrSproutIDNotFound  = errors.New("A Sprout ID matching that system cannot be found.")
-	ErrInvalidUserInput  = errors.New("Invalid user input was received")
+	ErrAPIRouteNotFound     = errors.New("API Route not found.")
+	ErrAlreadyAccepted      = errors.New("This Sprout ID was already accepted.")
+	ErrAlreadyDenied        = errors.New("This Sprout ID was already denied.")
+	ErrAlreadyRejected      = errors.New("This Sprout ID was already rejected.")
+	ErrAlreadyUnaccepted    = errors.New("This Sprout ID was already unaccepted.")
+	ErrCannotParseRootCA    = errors.New("Cannot load the RootCA certificate.")
+	ErrDependencyCycleFound = errors.New("Found a dependency cycle!")
+	ErrSproutIDFound        = errors.New("A Sprout ID matching that system has already been recorded.")
+	ErrSproutIDInvalid      = errors.New("Bad user input: invalid SproutID received")
+	ErrSproutIDNotFound     = errors.New("A Sprout ID matching that system cannot be found.")
+	ErrInvalidUserInput     = errors.New("Invalid user input was received")
 
 	ErrInvalidKeyState          = errors.New("Code bug: an invalid key state was supplied")
 	ErrConfirmationLengthIsZero = errors.New("Code bug: confirmation options muct not be 0-length")
