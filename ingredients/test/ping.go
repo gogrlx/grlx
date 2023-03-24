d937459fde3e8a2a96761fa3c719ca06560c18cb
Tai Groot <tai@taigrr.com>
2023-03-24 13:53:36 -0700

    follow more code recs

--- ingredients/test/ping.go
+++ ingredients/test/ping.go
@@ -3,21 +3,20 @@ package test
 import (
 	"time"
 
-	. "github.com/gogrlx/grlx/types"
+	"github.com/gogrlx/grlx/types"
 )
 
 // TODO allow selector to be more than an ID
-func FPing(target KeyManager, ping PingPong) (PingPong, error) {
-
+func FPing(target types.KeyManager, ping types.PingPong) (types.PingPong, error) {
 	topic := "grlx.sprouts." + target.SproutID + ".test.ping"
 	ping.Ping = true
 	ping.Pong = false
-	var pong PingPong
+	var pong types.PingPong
 	err := ec.Request(topic, ping, &pong, time.Second*15)
 	return pong, err
 }
 
-func SPing(ping PingPong) (PingPong, error) {
+func SPing(ping types.PingPong) (types.PingPong, error) {
 	ping.Pong = true
 	return ping, nil
 }
