a79315bf625eb31aaf7620098b3cab52db51e96b
Tai Groot <tai@taigrr.com>
2023-06-11 00:21:54 -0700

    prepping for ingredient api

--- cook/actuallycook.go
+++ cook/actuallycook.go
@@ -24,10 +24,6 @@ type StepCompletion struct {
 
 func CookRecipeEnvelope(envelope types.RecipeEnvelope) error {
 	completionMap := map[types.StepID]StepCompletion{}
-	stepMap := map[types.StepID]types.Step{}
-	for _, step := range envelope.Steps {
-		stepMap[step.ID] = step
-	}
 	for _, step := range envelope.Steps {
 		completionMap[step.ID] = StepCompletion{
 			ID:               step.ID,
@@ -35,41 +31,61 @@ func CookRecipeEnvelope(envelope types.RecipeEnvelope) error {
 			ChangesMade:      false,
 		}
 	}
+	stepMap := map[types.StepID]types.Step{}
+	for _, step := range envelope.Steps {
+		stepMap[step.ID] = step
+	}
+	// create a wait group and a channel to receive step completions
 	wg := sync.WaitGroup{}
 	wg.Add(len(envelope.Steps))
 	completionChan := make(chan StepCompletion, 5)
 	ctx, cancel := context.WithCancel(context.Background())
+	// spawn a goroutine to wait for all steps to complete and then cancel the context
 	go func() {
 		wg.Wait()
 		cancel()
 	}()
 	for {
 		select {
+		// each time a step completes, check if any other steps can be started
 		case completion := <-completionChan:
 			completionMap[completion.ID] = completion
 			for id, step := range completionMap {
 				if step.CompletionStatus != NotStarted {
 					continue
 				}
-				requisitesMet, err := RequisitesAreMet(stepMap[id])
+				requisitesMet, err := RequisitesAreMet(stepMap[id], completionMap)
 				if err != nil {
-					// TODO here broadcast the error on the bus and mark the step as failed
+					// TODO here broadcast the error on the bus
+					completionChan <- StepCompletion{
+						ID:               id,
+						CompletionStatus: Failed,
+					}
 					wg.Done()
+					continue
 				}
 				if !requisitesMet {
 					continue
 				}
-				go func() {
+				// all requisites are met, so start the step in a goroutine
+				go func(step types.Step) {
 					// TODO here use the ingredient package to load and cook the step
 					wg.Done()
-				}()
+					completionChan <- StepCompletion{
+						ID:               step.ID,
+						CompletionStatus: Completed,
+					}
+				}(stepMap[id])
 			}
+		// All steps are done, so context will be cancelled and we'll exit
 		case <-ctx.Done():
 			return nil
 		}
 	}
 }
 
-func RequisitesAreMet(step types.Step) (bool, error) {
+// RequisitesAreMet returns true if all of the requisites for the given step are met
+// TODO this is a stub
+func RequisitesAreMet(step types.Step, completionMap map[types.StepID]StepCompletion) (bool, error) {
 	return false, nil
 }
