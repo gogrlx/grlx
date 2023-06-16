37f200e239bf49435b5447ca203eafc59e5b07ce
Tai Groot <tai@taigrr.com>
2023-06-16 13:43:06 -0400

    add stub for local file provider

---
+++ ingredients/file/local/provider.go
@@ -0,0 +1,48 @@
+package local
+
+import (
+	"context"
+
+	"github.com/gogrlx/grlx/ingredients/file"
+	"github.com/gogrlx/grlx/types"
+)
+
+//	FileProvider interface {
+//		Download(context.Context) error
+//		Properties() (map[string]interface{}, error)
+//		Parse(id, source, destination, hash string, properties map[string]interface{}) (FileProvider, error)
+//		Protocols() []string
+//		Verify(context.Context) (bool, error)
+//	}
+
+type LocalFile struct {
+	ID          string
+	Source      string
+	Destination string
+	Hash        string
+	Props       map[string]interface{}
+}
+
+func (lf LocalFile) Download(context.Context) error {
+	return nil
+}
+
+func (lf LocalFile) Properties() (map[string]interface{}, error) {
+	return lf.Props, nil
+}
+
+func (lf LocalFile) Parse(id, source, destination, hash string, properties map[string]interface{}) (types.FileProvider, error) {
+	return LocalFile{ID: id, Source: source, Destination: destination, Hash: hash, Props: properties}, nil
+}
+
+func (lf LocalFile) Protocols() []string {
+	return []string{"file"}
+}
+
+func (lf LocalFile) Verify(context.Context) (bool, error) {
+	return false, nil
+}
+
+func init() {
+	file.RegisterProvider(LocalFile{})
+}
