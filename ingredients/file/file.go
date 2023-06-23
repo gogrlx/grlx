bac3dc119554ce9a5985ae6bb64c29deebd0c89b
Tai Groot <tai@taigrr.com>
2023-06-22 17:31:13 -0700

    update to add chown functionality to directory

--- ingredients/file/file.go
+++ ingredients/file/file.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
+	"os/user"
 	"path/filepath"
+	"strconv"
 
 	"github.com/gogrlx/grlx/config"
 	"github.com/gogrlx/grlx/ingredients"
@@ -42,7 +45,6 @@ func (f File) Test(ctx context.Context) (types.Result, error) {
 		return f.undef()
 	case "directory":
 		return f.directory(ctx, true)
-		return f.undef()
 	case "missing":
 		return f.undef()
 	case "prepend":
@@ -108,6 +110,20 @@ func (f File) cached(ctx context.Context, test bool) (types.Result, error) {
 }
 
 func (f File) directory(ctx context.Context, test bool) (types.Result, error) {
+	type dir struct {
+		user           string
+		group          string
+		recurse        bool
+		maxDepth       int
+		dirMode        string
+		fileMode       string
+		makeDirs       bool
+		clean          bool
+		followSymlinks bool
+		force          bool
+		backupName     string
+		allowSymlink   bool
+	}
 	name, ok := f.params["name"].(string)
 	if !ok {
 		return types.Result{Succeeded: false, Failed: true}, types.ErrMissingName
@@ -118,6 +134,69 @@ func (f File) directory(ctx context.Context, test bool) (types.Result, error) {
 	}
 	if name == "/" {
 		return types.Result{Succeeded: false, Failed: true}, fmt.Errorf("refusing to delete root")
+	}
+	d := dir{}
+	// chown the directory to the named user
+	{
+		if val, ok := f.params["user"].(string); ok {
+			d.user = val
+			userL, lookupErr := user.Lookup(d.user)
+			if lookupErr != nil {
+				return types.Result{Succeeded: false, Failed: true}, lookupErr
+			}
+			uid, parseErr := strconv.ParseUint(userL.Uid, 10, 32)
+			if parseErr != nil {
+				return types.Result{Succeeded: false, Failed: true}, parseErr
+			}
+
+			chownErr := os.Chown(name, int(uid), -1)
+			if chownErr != nil {
+				return types.Result{Succeeded: false, Failed: true}, chownErr
+			}
+			if val, ok := f.params["recurse"].(bool); ok && val {
+				walkErr := filepath.WalkDir(name, func(path string, d fs.DirEntry, err error) error {
+					return os.Chown(path, int(uid), -1)
+				})
+				if walkErr != nil {
+					return types.Result{Succeeded: false, Failed: true}, walkErr
+				}
+			}
+		}
+	}
+	// chown the directory to the named group
+	{
+		if val, ok := f.params["group"].(string); ok {
+			d.group = val
+			group, lookupErr := user.LookupGroup(d.group)
+			if lookupErr != nil {
+				return types.Result{Succeeded: false, Failed: true}, lookupErr
+			}
+			gid, parseErr := strconv.ParseUint(group.Gid, 10, 32)
+			if parseErr != nil {
+				return types.Result{Succeeded: false, Failed: true}, parseErr
+			}
+			chownErr := os.Chown(name, -1, int(gid))
+			if chownErr != nil {
+				return types.Result{Succeeded: false, Failed: true}, chownErr
+			}
+			if val, ok := f.params["recurse"].(bool); ok && val {
+				walkErr := filepath.WalkDir(name, func(path string, d fs.DirEntry, err error) error {
+					return os.Chown(path, -1, int(gid))
+				})
+				if walkErr != nil {
+					return types.Result{Succeeded: false, Failed: true}, walkErr
+				}
+			}
+		}
+	}
+	modeVal, _ := strconv.ParseUint(d.dirMode, 8, 32)
+	os.MkdirAll(name, 0o755)
+	//"user": "string", "group": "string", "recurse": "bool",
+	//"max_depth": "int", "dir_mode": "string", "file_mode": "string", "makedirs": "bool",
+	//"clean": "bool", "follow_symlinks": "bool", "force": "bool", "backupname": "string", "allow_symlink": "bool",
+	err := os.Chmod(name, os.FileMode(modeVal))
+	if err != nil {
+		return types.Result{Succeeded: false, Failed: true}, err
 	}
 	return f.undef()
 }
